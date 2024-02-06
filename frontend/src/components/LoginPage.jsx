84004218075a5e32f4d2b18a8b44b8071ecf0767
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-06 12:45:49 +0200

    Center form and image placeholder

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -4,40 +4,72 @@
 function LoginPage() {
     return (
       <>
-         <div id="content" class="full-width">   
-            
-            <div class="left column">
+         <div id="content" class="form" style={{display: "flex", justifyContent: "center" }}>       
+
+            <div class="left-column">
+
+                <div class="form-header">
+                    <h2>
+                        Login to your account
+                    </h2>
+                </div>
                 
-                <h2>Login to your account</h2>
-                    <label>
-                        Email
+                    <div class="field-label">
+                        <label>
+                            Email
+                            
+                        </label>
+                    </div>
+
+                    <div class="field-input">
                         <input 
-                            type="email" 
-                            name="email" />
-                    </label>
-                    <label>
-                        Password
-                        <input 
-                            type="password" 
-                            name="password" />
-                    </label>
-                    <input 
-                        type="submit"
-                        value="Login" />
+                                    type="email" 
+                                    name="email" />
+                    </div>
                     
-                    <a 
-                        href="https://www.life-framer.com/password-reset/" 
-                        class="um-link-alt">
-                        Forgot your password?</a>
+                    <div class="field-label">
+                        <label>
+                            Password
+                        
+                        </label>
+                    </div>
+
+                    <div class="field-input">
+                        <input 
+                                type="password" 
+                                name="password" />
+                    </div>
+
+                    <div class="button-login">
+                        <input 
+                            type="submit"
+                            value="Login" />
+                    </div>
+                    <div class="link">
+                        <a 
+                            href="https://www.life-framer.com/password-reset/" 
+                            >
+                            Forgot your password?</a>
+                    </div>
+                    
                 
             </div>
 
                 <div class ="right column">
-                    Don't have an Account?	    
-                    <a 
+
+                    <div class="form-header">
+                        <p>
+                            Don't have an Account?
+                        </p>
+                    </div>
+
+                    <div class="link">
+                     <a 
                         href="https://www.life-framer.com/password-reset/"
                         class="um-link-alt">
                         Register</a>
+                    </div>
+                 
                 </div>
 
         </div>
