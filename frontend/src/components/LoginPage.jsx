ba152d541e644a1129ffcd5a90700d087be67ab7
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-06 11:39:11 +0200

    Updated form header button register forgot password links

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -4,25 +4,43 @@
 function LoginPage() {
     return (
       <>
-        
-        <form>
-            <label>
-                Email
-                <input 
-                    type="email" 
-                    name="email" />
-            </label>
-            <label>
-                Password
-                <input 
-                    type="password" 
-                    name="password" />
-            </label>
-            <input 
-                type="submit"
-                value="submit" />
-        </form>
+         <div id="content" class="full-width">   
+            
+            <div class="left column">
+                
+                <h2>Login to your account</h2>
+                    <label>
+                        Email
+                        <input 
+                            type="email" 
+                            name="email" />
+                    </label>
+                    <label>
+                        Password
+                        <input 
+                            type="password" 
+                            name="password" />
+                    </label>
+                    <input 
+                        type="submit"
+                        value="Login" />
+                    
+                    <a 
+                        href="https://www.life-framer.com/password-reset/" 
+                        class="um-link-alt">
+                        Forgot your password?</a>
+                
+            </div>
 
+                <div class ="right column">
+                    Don't have an Account?	    
+                    <a 
+                        href="https://www.life-framer.com/password-reset/"
+                        class="um-link-alt">
+                        Register</a>
+                </div>
+
+        </div>
       </>
     );
   }
