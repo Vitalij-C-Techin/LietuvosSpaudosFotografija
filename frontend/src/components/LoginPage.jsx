ed56e1f31fa52d9c9ce8b9cfa51a4275b7a04a55
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-06 13:37:53 +0200

    flex exported to external LoginPage.css

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,74 +1,76 @@
 
-
+import './LoginPage.css'
 
 function LoginPage() {
     return (
       <>
-         <div id="content" class="form" style={{display: "flex", justifyContent: "center" }}>       
-
-            <div class="left-column">
-
-                <div class="form-header">
-                    <h2>
-                        Login to your account
-                    </h2>
-                </div>
-                
-                    <div class="field-label">
-                        <label>
-                            Email
-                            
-                        </label>
-                    </div>
-
-                    <div class="field-input">
-                        <input 
-                                    type="email" 
-                                    name="email" />
-                    </div>
+         <div className="container">       
+          
+            <div className="left-column">
+                <div className="form">
+                        <div className="form-header">
+                            <h2>
+                                Login to your account
+                            </h2>
+                        </div>
                     
-                    <div class="field-label">
-                        <label>
-                            Password
+                        <div className="field-label">
+                            <label>
+                                Email
+                                
+                            </label>
+                        </div>
+
+                        <div className="field-input">
+                            <input 
+                                        type="email" 
+                                        name="email" />
+                        </div>
                         
-                        </label>
-                    </div>
-
-                    <div class="field-input">
-                        <input 
-                                type="password" 
-                                name="password" />
-                    </div>
-
-                    <div class="button-login">
-                        <input 
-                            type="submit"
-                            value="Login" />
-                    </div>
-                    <div class="link">
-                        <a 
-                            href="https://www.life-framer.com/password-reset/" 
-                            >
-                            Forgot your password?</a>
-                    </div>
+                        <div className="field-label">
+                            <label>
+                                Password
+                            
+                            </label>
+                        </div>
+                      
+                        <div className="field-input">
+                            <input 
+                                    type="password" 
+                                    name="password" />
+                        </div>
+                       
+                        <div className="button-login">
+                            <input 
+                                type="submit"
+                                value="Login" />
+                        </div>
+                        <div className="link">
+                            <a 
+                                href="https://www.life-framer.com/password-reset/" 
+                                >
+                                Forgot your password?</a>
+                        </div>
                     
-                
+                </div>
             </div>
 
-                <div class ="right column">
+                <div className ="right-column" style={{}}>
+                    <div className="text-wrapper" style={{borderStyle:"solid"}}>
+                        <div className="form-header">
+                            <p>
+                                Don't have an Account?
+                            </p>
+                        </div>
 
-                    <div class="form-header">
-                        <p>
-                            Don't have an Account?
-                        </p>
+                        <div className="link">
+                        <a 
+                            href="https://www.life-framer.com/password-reset/"
+                           >
+                            Register</a>
+                        </div>
                     </div>
-
-                    <div class="link">
-                     <a 
-                        href="https://www.life-framer.com/password-reset/"
-                        class="um-link-alt">
-                        Register</a>
-                    </div>
+                    
                  
                 </div>
 
