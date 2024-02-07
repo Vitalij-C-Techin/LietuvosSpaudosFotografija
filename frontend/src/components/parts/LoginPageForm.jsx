db060605a8f63dc560ddd8654321e8354bba109f
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-07 17:10:40 +0200

    Add inline styles

--- frontend/src/components/parts/LoginPageForm.jsx
+++ frontend/src/components/parts/LoginPageForm.jsx
@@ -6,42 +6,47 @@ import Button from 'react-bootstrap/Button';
 function LoginPageForm() {
   return (
     <>  
+         <div style={{padding:"50px"}}>
+            <h2>Log in to your account</h2>
+
+            <Form style={{margin:"auto", width: "30%", textAlign:"center"}}>    
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email"  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password"  />
+                </Form.Group>
+
+                <Form.Group  className="mb-3" controlId="formGroupLinks">
+                    <Row className="align-items-center">
+                        <Col>
+                        <div style={{ textAlign: "left" }}>
+                            <a href="url">Forgot password?</a>
+                        </div>
+                        </Col>
+                    </Row>
+                
+                </Form.Group>
         
-        <Form style={{margin:"auto", width: "30%"}}>
-
-            <Form.Group style={{textAlign:"center"}} className="mb-3" controlId="formGroupHeadline">
-            <h2>Log in to your Account</h2>
-            </Form.Group>
-            
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email"  />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"  />
-            </Form.Group>
-
-            <Form.Group style={{textAlign:"center"}} className="mb-3" controlId="formGroupLinks">
-                <Row className="align-items-center">
-                    <Col>
-                        <a href="url">Forgot password?</a>
-                    </Col>
-                    <Col>
-                        <a href="url">Register</a>
-                    </Col>
-                </Row>
-              
-            </Form.Group>
-      
-            <Form.Group style={{margin:"auto", width: "50%"}} className="d-grid gap-2" controlId="formGroupButton">
-            <Button  variant="light" type="login">
-                Login
-            </Button>
-            </Form.Group>
-        </Form>
-
+                <Form.Group className="mb-3" controlId="formGroupButton">
+                    <Row className="align-items-center">
+                        <Col  xs={12} md={6}>
+                            <Button  variant="light" type="login" style={{ width: "100%" }}>
+                                Login
+                            </Button>
+                        </Col>
+                        <Col  xs={12} md={6}>
+                            <Button  variant="light" type="login" style={{ width: "100%" }}>
+                                Register
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form.Group>
+            </Form>
+        </div>
     </>
   );
 }
