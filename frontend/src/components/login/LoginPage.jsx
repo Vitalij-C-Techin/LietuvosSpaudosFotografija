3fc9921b14b84161e93132e6f4e39ed308a9f355
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-07 12:38:01 +0200

    add seperate folder for login within components transfer LoginPage files

---
+++ frontend/src/components/login/LoginPage.jsx
@@ -0,0 +1,90 @@
+
+import './LoginPage.css'
+
+function LoginPage() {
+    return (
+      <>
+         <div className="container">       
+          
+            <div className="left-column">
+             
+                <div className="form">
+                
+                        <div className="form-header">
+                            <h2>
+                                Login to your account
+                            </h2>
+                        </div>
+                    
+                        <div className="field-label">
+                            <label>
+                                Email
+                                
+                            </label>
+                        </div>
+
+                        <div className="field-input">
+                            <input 
+                                        className="field"
+                                        type="email" 
+                                        name="email" />
+                        </div>
+                        
+                        <div className="field-label">
+                            <label>
+                                Password
+                            
+                            </label>
+                        </div>
+                      
+                        <div className="field-input">
+                            <input 
+                                className="field"
+                                    type="password" 
+                                    name="password" />
+                        </div>
+                        <div className="link">
+                            <a 
+                                href="" 
+                                >
+                                Forgot your password?</a>
+                        </div>
+                        <div className="button-login" style={{display:"block"}}>
+                            <input 
+                             
+                                type="submit"
+                                value="Login" />
+                        </div>
+                   
+                     
+                </div>
+            </div>
+
+                <div className ="right-column">
+                    <div className='background-picture' style={{borderStyle:"solid"}}>
+                        <div className="text-wrapper" >
+                            <div className="form-header">
+                                <p>
+                                    Don't have an account yet?
+                                </p>
+                            </div>
+
+                            <div className="link">
+                            <a 
+                                href=""
+                            >
+                                Register</a>
+                            </div>
+                        </div>
+                    </div>
+                  
+                    
+                 
+                </div>
+
+        </div>
+      </>
+    );
+  }
+  
+  export default LoginPage;
