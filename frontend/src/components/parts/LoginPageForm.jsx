ec222c1f5e797fa6b44843484d03e84e9459ac90
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-07 13:28:48 +0200

    change LoginPageForm layout to react bootstrap

--- frontend/src/components/parts/LoginPageForm.jsx
+++ frontend/src/components/parts/LoginPageForm.jsx
@@ -1,87 +1,18 @@
+import Form from 'react-bootstrap/Form';
 
+function LoginFormLayout() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>
+  );
+}
 
-
-function LoginPageForm() {
-    return (
-      <>
-         <div 
-         className="container">       
-          
-            <div 
-            className="left-column">
-             
-                <div 
-                className="form">
-                
-                        <div 
-                        className="form-header">
-                            <h2>
-                                Login to your account
-                            </h2>
-                        </div>
-                    
-                        <div 
-                        className="field-label">
-                            <label>
-                                Email
-                                
-                            </label>
-                        </div>
-
-                        <div 
-                        className="field-input">
-                            <input 
-                                        className="field"
-                                        type="email" 
-                                        name="email" />
-                        </div>
-                        
-                        <div 
-                        className="field-label">
-                            <label>
-                                Password
-                            
-                            </label>
-                        </div>
-                      
-                        <div 
-                        className="field-input">
-                            <input 
-                                className="field"
-                                    type="password" 
-                                    name="password" />
-                        </div>
-                        <div 
-                        className="link"
-                        style={{display:"block"}}>
-                            <a 
-                                href="" 
-                                >
-                                Forgot your password?</a>
-                        </div>
-                        <div 
-                        className="link"
-                        style={{display:"block"}}>
-                            <a 
-                                href=""
-                            >
-                                Register</a>
-                            </div>
-                        <div 
-                        className="button-login" style={{display:"block"}}>
-                            <input 
-                             
-                                type="submit"
-                                value="Login" />
-                        </div>
-                   
-                     
-                </div>
-            </div>
-
-        </div>
-      </>
-    );
-  }
-  
-  export default LoginPageForm;
+export default LoginFormLayout;
