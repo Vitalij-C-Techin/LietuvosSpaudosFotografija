8b569ec767e8a2f2e3a6612f9040e24992bb06bf
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-08 12:30:47 +0200

    add ForgotPasswordPage to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import LanguageSwitch from './components/language-switch';
+import './css/style.css';
+import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
 
 function App() {
   const { t } = useTranslation();
@@ -8,6 +10,7 @@ function App() {
     <>
       <LanguageSwitch />
       <h3>{t('description.part1')}</h3>
+      <ForgotPasswordPage />
     </>
   );
 }
