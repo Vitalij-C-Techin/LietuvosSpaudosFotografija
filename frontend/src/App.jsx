4e5bbbfc8fb5bf0ae0cc42ec84b1a463f1d566fa
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-08 12:35:13 +0200

    add bootstrap dependency import to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import LanguageSwitch from './components/language-switch';
-import './css/style.css';
 import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const { t } = useTranslation();
