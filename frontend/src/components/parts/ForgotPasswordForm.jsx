8ae914da79a2e26e31522760af4101e074f702ad
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-17 17:40:57 +0200

    align code

--- frontend/src/components/parts/ForgotPasswordForm.jsx
+++ frontend/src/components/parts/ForgotPasswordForm.jsx
@@ -43,36 +43,36 @@ const ForgotPasswordForm = () => {
 
   return (
     <>
-      <Container className="fpassword-form-container justify-content-md-center">
-        <Row>
-          <Col>
-          <Card className="my-5">
-        <h2>{t('forgotPasswordForm.resetPassword')}</h2>
-        {message && <p>{message}</p>}
-        </Card>
-        <Form onSubmit={handleSubmit} noValidate>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Control
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder={t('forgotPasswordForm.formPlaceholderText')}
-              data-testid="email-input"
-            />
-            {errors.email && <p className="text-danger">{errors.email}</p>}
-          </Form.Group>
+      <Container className="fpassword-form-container">
+        <Row className="justify-content-md-center">
+          <Col xs="12" sm="8" md="6" lg="4">
+            <Card className="my-5">
+              <h2>{t('forgotPasswordForm.resetPassword')}</h2>
+              {message && <p>{message}</p>}
+            </Card>
+            <Form onSubmit={handleSubmit} noValidate>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  placeholder={t('forgotPasswordForm.formPlaceholderText')}
+                  data-testid="email-input"
+                />
+                {errors.email && <p className="text-danger">{errors.email}</p>}
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formGroupButton">
-            <Row className="align-items-center">
-              <Col>
-                <Button type="submit" data-testid="recover-button">
-                  {t('forgotPasswordForm.recoverButton')}
-                </Button>
-              </Col>
-            </Row>
-          </Form.Group>
-        </Form>
-        </Col>
+              <Form.Group className="mb-3" controlId="formGroupButton">
+                <Row className="align-items-center">
+                  <Col>
+                    <Button type="submit" data-testid="recover-button">
+                      {t('forgotPasswordForm.recoverButton')}
+                    </Button>
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </>
@@ -80,5 +80,3 @@ const ForgotPasswordForm = () => {
 };
 
 export default ForgotPasswordForm;
-
-
