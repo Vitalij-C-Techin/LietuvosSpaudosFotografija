b42020de7d3622baad68024aa9448c7b0f93a7d2
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-19 10:57:36 +0200

    Add LoginForm design  to translatated version

--- frontend/src/components/parts/LoginForm.jsx
+++ frontend/src/components/parts/LoginForm.jsx
@@ -43,72 +43,62 @@ const LoginForm = (onLogin) => {
   };
   return (
     <>
-      <section>
-        <h2 style={{ textAlign: 'center' }}>{t('loginPage.title')}</h2>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-
       <Container className="form-container justify-content-md-center">
         <Row className="justify-content-md-center">
-        <Col xs="12" sm ="8" md="6" lg="4">
-        <Card className="my-5">
-          {/* <Card.Body> */}
-            <h2>LOG IN TO </h2>
-            <h2>TO YOUR ACCOUNT</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-          {/* </Card.Body> */}
-        </Card>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label>{t('loginPage.email')}</Form.Label>
-            <Form.Control
-              type="email"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="egzamle@egzample.com"
-            />
-          </Form.Group>
+          <Col xs="12" sm="8" md="6" lg="4">
+            <Card className="my-5">
+              {/* <Card.Body> */}
+              <h2 style={{ textAlign: 'center' }}>{t('loginPage.title')}</h2>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+              {/* </Card.Body> */}
+            </Card>
 
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label>{t('loginPage.password')}</Form.Label>
-            <Form.Control
-              type="password"
-              autoComplete="new-password"
-              placeholder={t('registrationPage.passwordPlaceholder')}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>{t('loginPage.email')}</Form.Label>
+                <Form.Control
+                  type="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="egzamle@egzample.com"
+                />
+              </Form.Group>
 
-          <Form.Group controlId="formGroupLinks">
-            <Row className="align-items-center">
-              <Link to="/forgotpassword">{t('loginPage.forgotPassword')}</Link>
-            </Row>
-          </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>{t('loginPage.password')}</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="new-password"
+                  placeholder={t('registrationPage.passwordPlaceholder')}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formGroupButton">
-            <Row className="align-items-center">
-              <Col xs={12} md={6}>
-                <Button variant="light" type="submit">
-                  {t('loginPage.login')}
-                </Button>
-              <Col className="mt-3"  xs={12} md={6}>
-                <Button type="submit">LOGIN</Button>
-              </Col>
+              <Form.Group controlId="formGroupLinks">
+                <Row className="align-items-center">
+                  <Link to="/forgotpassword">{t('loginPage.forgotPassword')}</Link>
+                </Row>
+              </Form.Group>
 
-              <Col xs={12} md={6}>
-                <Button variant="light" type="button" onClick={navigateToRegistrationPage}>
-                  {t('loginPage.register')}
-              <Col className="mt-3"  xs={12} md={6} >
-                <Button type="button" onClick={navigateToRegistrationPage}>
-                  REGISTER
-                </Button>
-              </Col>
-            </Row>
-            
-          </Form.Group>
-        </Form>
-        </Col>
+              <Form.Group className="mb-3" controlId="formGroupButton">
+                <Row className="align-items-center">
+                  <Col className="mt-3" xs={12} md={6}>
+                    <Button variant="light" type="submit">
+                      {t('loginPage.login')}
+                    </Button>
+                  </Col>
+
+                  <Col className="mt-3" xs={12} md={6}>
+                    <Button variant="light" type="button" onClick={navigateToRegistrationPage}>
+                      {t('loginPage.register')}
+                    </Button>
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </>
