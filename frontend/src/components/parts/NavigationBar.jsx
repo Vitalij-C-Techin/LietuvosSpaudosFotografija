56a607c6cece86645dbfcadac1be5987112a3ad8
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-02-20 15:00:21 +0200

    add navbar show/hide when mouse is scrolled over/off and stay on when clicked on language selector

--- frontend/src/components/parts/NavigationBar.jsx
+++ frontend/src/components/parts/NavigationBar.jsx
@@ -7,16 +7,26 @@ import { useAuth } from '../../modules/AuthContext.jsx';
 import { Nav, Navbar, NavDropdown, Dropdown, Row, Container } from 'react-bootstrap';
 
 const NavigationBar = () => {
-  const [showNestedDropdown, setShowNestedDropdown] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
   const { isLoggedIn, logout } = useAuth();
 
   const { t } = useTranslation();
 
+  const handleMouseEnter = () => {
+    setShowDropdown(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowDropdown(false);
+  };
   return (
     <Navbar className="bg-body-tertiary" data-testid="NavigationBar">
       <Container>
         <Nav className="ms-auto">
-          <Dropdown>
+          <Dropdown
+                show={showDropdown}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
             <Dropdown.Toggle
               id="dropdown-basic"
               data-testid="dropdown-menu-button"
