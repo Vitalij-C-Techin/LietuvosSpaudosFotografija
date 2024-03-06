2b2bcf8e48146af37035fe4e577374bd5e8463f1
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-06 15:55:20 +0200

    fix input styling button

--- frontend/src/components/parts/ViewEditCompetitionForm.jsx
+++ frontend/src/components/parts/ViewEditCompetitionForm.jsx
@@ -62,96 +62,96 @@ const ViewEditCompetitionForm = ({ competitionData, onUpdate }) => {
 
   return (
     <>
-      <Container className="justify-content-xl-center my-5">
-        <Card className="image-header-text">
-          <h2>{t('editcomp.header')}</h2>
-        </Card>
-        <Button variant="secondary" className="lsf-Button w-40">
-          {t('editcomp.delete')}
-        </Button>
-      </Container>
-      <Container className="justify-content-xl-center my-5">
-        <Card>
+      <Container className="view-edit-competition-container">
+        <Container className="justify-content-xl-center my-5">
+          <Card className="image-header-text">
+            <h2>{t('editcomp.header')}</h2>
+          </Card>
+          <Button variant="secondary" className="lsf-Button w-40">
+            {t('editcomp.delete')}
+          </Button>
+        </Container>
+        <Container className="justify-content-xl-center my-5">
           <Form.Label htmlFor="cname">{t('editcomp.name')}</Form.Label>
-        </Card>
-        <Form.Control
-          type="text"
-          id="cname"
-          name="cname"
-          value={formData.cname}
-          onChange={handleInputChange}
-        />
-        <Form.Label htmlFor="description">{t('editcomp.description')}</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="description"
-          id="description"
-          value={formData.description}
-          onChange={handleInputChange}
-        ></Form.Control>
 
-       
-        <Form.Label htmlFor="StartDate">{t('editcomp.Sdate')}</Form.Label>
-        <Form.Control
-          type="date"
-          id="StartDate"
-          name="StartDate"
-          value={formData.StartDate}
-          onChange={handleInputChange}
-        />
-        <Form.Label htmlFor="photoLimit">{t('editcomp.Plimit')}</Form.Label>
-        <Form.Select
-          name="photoLimit"
-          id="photoLimit"
-          value={formData.photoLimit}
-          onChange={handleInputChange}
-        >
-          <option value="1">{t('editcomp.solo')}</option>
-          <option value="2">{t('editcomp.number')}</option>
-          <option value="3">{t('editcomp.group')}</option>
-        </Form.Select>
-        <Form.Label htmlFor="status">{t('editcomp.status')}</Form.Label>
-        <Form.Select
-          id="status"
-          name="status"
-          value={selectedStatus}
-          onChange={(e) => setSelectedStatus(e.target.value)}
-        >
-          <option value=""></option>
-          <option value="active">{t('editcomp.active')}</option>
-          <option value="closed">{t('editcomp.closed')}</option>
-        </Form.Select>
-        <Form.Label htmlFor="visibility">{t('editcomp.visible')}</Form.Label>
-        <Form.Select
-          name="visibility"
-          id="visibility"
-          value={formData.visibility}
-          onChange={handleInputChange}
-        >
-          <option value="1">{t('editcomp.active2')}</option>
-          <option value="2">{t('editcomp.closed2')}</option>
-        </Form.Select>
-        <Form.Label htmlFor="EndDate">{t('editcomp.Edate')}</Form.Label>
-        <Form.Control type="date" id="EndDate" name="EndDate" />
-      </Container>
-      <Container className="justify-content-xl-center my-5">
-        <h6>{t('editcomp.Addcategory')}</h6>
-      </Container>
-      <Container className="justify-content-xl-center my-5">
-        <Button variant="secondary" onClick={handleSave} disabled={!isFormChanged}>
-          {t('editcomp.Save')}
-        </Button>
-        <Button variant="secondary" onClick={handleCreateCategory}>
-          {t('editcomp.Ccategory')}
-        </Button>
-        <Button variant="secondary" onClick={handleAddCategory}>
-          {t('editcomp.Acategory')}
-        </Button>
-        <ModalCreateCategory
-          showModal={showCreateCategoryModal}
-          onClose={handleCloseCreateCategoryModal}
-        />
-        <ModalCategory showModal={showAddCategoryModal} onClose={handleCloseAddCategoryModal} />
+          <Form.Control
+            type="text"
+            id="cname"
+            name="cname"
+            value={formData.cname}
+            onChange={handleInputChange}
+          />
+          <Form.Label htmlFor="description">{t('editcomp.description')}</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="description"
+            id="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          ></Form.Control>
+
+          <Form.Label htmlFor="StartDate">{t('editcomp.Sdate')}</Form.Label>
+          <Form.Control
+            type="date"
+            id="StartDate"
+            name="StartDate"
+            value={formData.StartDate}
+            onChange={handleInputChange}
+          />
+          <Form.Label htmlFor="photoLimit">{t('editcomp.Plimit')}</Form.Label>
+          <Form.Select
+            name="photoLimit"
+            id="photoLimit"
+            value={formData.photoLimit}
+            onChange={handleInputChange}
+          >
+            <option value="1">{t('editcomp.solo')}</option>
+            <option value="2">{t('editcomp.number')}</option>
+            <option value="3">{t('editcomp.group')}</option>
+          </Form.Select>
+          <Form.Label htmlFor="status">{t('editcomp.status')}</Form.Label>
+          <Form.Select
+            id="status"
+            name="status"
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="active">{t('editcomp.active')}</option>
+            <option value="closed">{t('editcomp.closed')}</option>
+          </Form.Select>
+          <Form.Label htmlFor="visibility">{t('editcomp.visible')}</Form.Label>
+          <Form.Select
+            name="visibility"
+            id="visibility"
+            value={formData.visibility}
+            onChange={handleInputChange}
+          >
+            <option value="1">{t('editcomp.active2')}</option>
+            <option value="2">{t('editcomp.closed2')}</option>
+          </Form.Select>
+          <Form.Label htmlFor="EndDate">{t('editcomp.Edate')}</Form.Label>
+          <Form.Control type="date" id="EndDate" name="EndDate" />
+        </Container>
+        <Container className="justify-content-xl-center my-5">
+          <h6>{t('editcomp.Addcategory')}</h6>
+        </Container>
+        <Container className="justify-content-xl-center my-5">
+          <Button variant="secondary" onClick={handleSave} disabled={!isFormChanged}>
+            {t('editcomp.Save')}
+          </Button>
+          <Button variant="secondary" onClick={handleCreateCategory}>
+            {t('editcomp.Ccategory')}
+          </Button>
+          <Button variant="secondary" onClick={handleAddCategory}>
+            {t('editcomp.Acategory')}
+          </Button>
+          <ModalCreateCategory
+            showModal={showCreateCategoryModal}
+            onClose={handleCloseCreateCategoryModal}
+          />
+          <ModalCategory showModal={showAddCategoryModal} onClose={handleCloseAddCategoryModal} />
+        </Container>
       </Container>
     </>
   );
