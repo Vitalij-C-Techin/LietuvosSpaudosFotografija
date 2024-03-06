0e2f63f304ebf557a42212f3c9bbb1b1fcd476b0
Killster <dmitrijmick@gmail.com>
2024-03-06 23:38:31 +0200

    made ForgotPasswordForm test independent to language

--- frontend/src/components/__tests__/ForgotPasswordForm.test.jsx
+++ frontend/src/components/__tests__/ForgotPasswordForm.test.jsx
@@ -2,6 +2,8 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import axios from 'axios';
 import ForgotPasswordForm from '../parts/ForgotPasswordForm';
+import i18n from '../../modules/language/i18n';
+import { I18nextProvider } from 'react-i18next';
 
 jest.mock('axios');
 
@@ -23,11 +25,15 @@ test('displays success message on successful email submission', async () => {
 });
 
 test('displays error message on unsuccessful email submission', async () => {
-  const errorMessage = 'Klaida siunčiant slaptažodžio atkūrimo el. laišką.';
+  const errorMessage = 'forgotPasswordForm.emailSendingError';
   const mockError = new Error(errorMessage);
   axios.post.mockRejectedValue(mockError);
 
-  render(<ForgotPasswordForm />);
+  render(
+    <I18nextProvider i18n={i18n}>
+      <ForgotPasswordForm />
+    </I18nextProvider>
+  );
 
   const emailInput = screen.getByTestId('email-input');
   fireEvent.change(emailInput, { target: { value: 'test@mail.com' } });
@@ -37,6 +43,6 @@ test('displays error message on unsuccessful email submission', async () => {
   await waitFor(() => {
     const errorMsg = screen.getByTestId('error-message');
     expect(errorMsg).toBeInTheDocument();
-    expect(errorMsg.textContent).toContain(errorMessage);
+    expect(errorMsg.textContent).toContain(i18n.t(errorMessage));
   });
 });
