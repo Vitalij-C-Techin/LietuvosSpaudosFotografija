c7c9c8da68857d69589278fa8acdb48564bff175
Valdemaras <valdemarasged@gmail.com>
2024-03-13 10:04:50 +0200

    Refactor EmailService and PasswordResetService, add email message

--- backend/src/main/java/lt/techin/lsf/service/PasswordResetService.java
+++ backend/src/main/java/lt/techin/lsf/service/PasswordResetService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lt.techin.lsf.model.requests.ForgetPasswordRequest;
 import lt.techin.lsf.persistance.UserRepository;
 import lt.techin.lsf.persistance.model.UserRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,30 +19,26 @@ public class PasswordResetService {
 
     private final UserRepository userRepository;
 
-
     public ResponseEntity<String> resetPassword(ForgetPasswordRequest forgetPasswordRequest) {
         String email = forgetPasswordRequest.getEmail();
         if (userRepository.existsByEmail(email)) {
             UserRecord user = userRepository.findByEmailIgnoreCase(email);
             initializePasswordReset(user);
             userRepository.save(user);
-            String emailChangeLink = "http://localhost:5173/change-password?token=" + user.getPasswordResetToken();
-            emailService.sendMailUsingMailjet(email, "email reset link", emailChangeLink);
-            return new ResponseEntity<>("If the user exists in our database, an email link will be sent."
-                    , HttpStatus.ACCEPTED);
+            String emailMessage = EmailMessage.builder()
+                    .passwordResetLink("http://localhost:5173/change-password?token="
+                            + user.getPasswordResetToken())
+                    .emailRecipient(user.getName()).build().getEmailMessage();
+            emailService.sendMailUsingMailjet(email, "email reset link", emailMessage);
         }
-        else {
-            return new ResponseEntity<>("If the user exists in our database, an email link will be sent."
-                    , HttpStatus.ACCEPTED);
-        }
+        return new ResponseEntity<>("If the user exists in our database, an email link will be sent.",
+                HttpStatus.ACCEPTED);
     }
-
 
     public void initializePasswordReset(UserRecord existingUser) {
-            LocalDateTime now = LocalDateTime.now();
-            existingUser.setPasswordResetRequestAt(now);
-            String passwordResetToken = UUID.randomUUID().toString();
-            existingUser.setPasswordResetToken(passwordResetToken);
+        LocalDateTime now = LocalDateTime.now();
+        existingUser.setPasswordResetRequestAt(now);
+        String passwordResetToken = UUID.randomUUID().toString();
+        existingUser.setPasswordResetToken(passwordResetToken);
     }
 }
-
