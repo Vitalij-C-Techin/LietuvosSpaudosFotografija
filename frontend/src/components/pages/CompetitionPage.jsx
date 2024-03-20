232c74acb7ab17083291f0397852f8a2cb6ade68
Valdemaras <valdemarasged@gmail.com>
2024-03-20 09:14:11 +0200

    Add pagination and custom controls(vote button) to CompetitionPage

--- frontend/src/components/pages/CompetitionPage.jsx
+++ frontend/src/components/pages/CompetitionPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { Container, Image, Col, Row, Card } from 'react-bootstrap';
+import { Container, Image, Col, Row, Card, Pagination, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import imagePlaceHolder from '../../images/image.jpg';
 import ImageGallery from 'react-image-gallery';
@@ -29,13 +29,28 @@ const CompetitionPage = () => {
       description: '5'
     },
     {
-      original: 'https://picsum.photos/id/1019/1000/600/',
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      description: '6'
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      description: '4'
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      description: '5'
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
       description: '6'
     }
   ];
   const [showGallery, setShowGallery] = useState(false);
   const toggleFullScreenRef = useRef();
   const [clickedImageIndex, setClickedImageIndex] = useState(null);
+  const [activePage, setActivePage] = useState(1);
+  const itemsPerPage = 3;
+  const totalPages = Math.ceil(images.length / itemsPerPage);
 
   useEffect(() => {
     if (showGallery) {
@@ -54,8 +69,35 @@ const CompetitionPage = () => {
     setClickedImageIndex(index);
   };
 
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
+
+  const getPaginatedImages = () => {
+    const startIndex = (activePage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return images.slice(startIndex, endIndex);
+  };
+
+  const renderCustomControls = () => {
+    const handleButtonClick = () => {
+      console.log('Custom button clicked!');
+    };
+
+    return (
+      <div
+        className="custom-controls-container"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <Button variant="primary" onClick={handleButtonClick}>
+          Custom Button
+        </Button>
+      </div>
+    );
+  };
+
   return (
-    <Container className="competition-container">
+    <Container className="competition-container my-2">
       <Container>
         <Row>
           <Col xxl="2" xl="2" lg="2" md="3" sm="3" className="d-flex justify-content-center">
@@ -77,9 +119,9 @@ const CompetitionPage = () => {
         </Row>
       </Container>
       <div className="divider"></div>
-      <Row>
-        {images.map((image, index) => (
-          <Col key={index}>
+      <Row className="mx-1">
+        {getPaginatedImages().map((image, index) => (
+          <Col key={index} xl="4" className="my-3">
             <Card>
               <Image
                 src={image.original}
@@ -103,8 +145,21 @@ const CompetitionPage = () => {
           startIndex={clickedImageIndex}
           onScreenChange={handleScreenChange}
           autoPlay={false}
+          renderCustomControls={renderCustomControls}
         />
       )}
+
+      <Pagination className="mt-3 justify-content-center custom-pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Pagination.Item
+            key={index + 1}
+            active={activePage === index + 1}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Pagination.Item>
+        ))}
+      </Pagination>
     </Container>
   );
 };
