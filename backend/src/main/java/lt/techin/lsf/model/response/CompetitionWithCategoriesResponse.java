e10d9c8a9130af2713ecf68035e334476f429fe0
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-03-20 14:13:46 +0200

    Updated to show competition with all its categories.

--- backend/src/main/java/lt/techin/lsf/model/response/CompetitionWithCategoriesResponse.java
+++ backend/src/main/java/lt/techin/lsf/model/response/CompetitionWithCategoriesResponse.java
@@ -3,15 +3,11 @@ package lt.techin.lsf.model.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lt.techin.lsf.persistance.model.CategoryRecord;
 import lt.techin.lsf.persistance.model.CompetitionRecord;
-
-import java.util.List;
 
 @Getter
 @Setter
 @Builder
 public class CompetitionWithCategoriesResponse {
-    public CompetitionRecord competition;
-    public List<CategoryRecord> categories;
+    private CompetitionRecord competition;
 }
--- backend/src/main/java/lt/techin/lsf/model/response/CreateCompetitionResponse.java
+++ backend/src/main/java/lt/techin/lsf/model/response/CreateCompetitionResponse.java
@@ -3,15 +3,11 @@ package lt.techin.lsf.model.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lt.techin.lsf.model.Category;
 import lt.techin.lsf.persistance.model.CompetitionRecord;
-
-import java.util.List;
 
 @Getter
 @Setter
 @Builder
 public class CreateCompetitionResponse {
     private CompetitionRecord competition;
-    private List<Category> categories;
 }
