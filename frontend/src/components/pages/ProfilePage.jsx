e866114e0b28cb8adbce531a68ec82db28b0cfe5
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-21 18:38:59 +0200

    add rows and columns

--- frontend/src/components/pages/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import { useTransition } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { Button } from 'react-bootstrap';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Authentication, IsAuthenticated, IsNotAuthenticated } from '../utils/Authentication';
 
@@ -13,7 +13,14 @@ const ProfilePage = () => {
   return (
     <>
       <Container>
-        <h3>{t('profile.myProfile')}</h3>
+        <Row className="profile-header">
+          <Col>
+            <h3>{t('profile.myProfile')}</h3>
+          </Col>
+          <IsAuthenticated>
+            <Col className="profile-role">{getUserData().role}</Col>
+          </IsAuthenticated>
+        </Row>
 
         <IsNotAuthenticated>
           <>
@@ -24,38 +31,39 @@ const ProfilePage = () => {
         <IsAuthenticated>
           {!!getUserData() && (
             <>
-              <p>
-                {t('profile.id')}: {getUserData().uuid}
-              </p>
-              <p>
-                {t('profile.name')}: {getUserData().name}
-              </p>
-              <p>
-                {t('profile.surname')}: {getUserData().surname}
-              </p>
-              <p>
-                {t('profile.email')}: {getUserData().email}
-              </p>
-              <p>
-                {t('profile.role')}: {getUserData().role}
-              </p>
-              <p>
-                {t('profile.birthYear')}: {getUserData().birth_year}
-              </p>
-              <p>
-                {t('profile.phoneNumber')}: {getUserData().phone_number}
-              </p>
-              <p>
-                {t('profile.media')}: {getUserData().media_name}
-              </p>
-              <p>
-                {t('profile.createdAt')}: {getUserData().created_at}
-              </p>
+              <Row>
+                <Col>
+                  {t('profile.name')}: {getUserData().name}
+                </Col>
+                <Col>
+                  {t('profile.surname')}: {getUserData().surname}
+                </Col>
+                <Col>
+                  {t('profile.email')}: {getUserData().email}
+                </Col>
+                <Col>
+                  {t('profile.birthYear')}: {getUserData().birth_year}
+                </Col>
+                <Col>
+                  {t('profile.phoneNumber')}: {getUserData().phone_number}
+                </Col>
+                <Col>
+                  {t('profile.media')}: {getUserData().media_name}
+                </Col>
+              </Row>
             </>
           )}
           <Button as={NavLink} to="/profile/edit">
             {t('profile.editProfile')}
           </Button>
+          <Row>
+            <Col>
+              {t('profile.id')}: {getUserData().uuid}
+            </Col>
+            <Col>
+              {t('profile.createdAt')}: {getUserData().created_at}
+            </Col>
+          </Row>
         </IsAuthenticated>
       </Container>
     </>
