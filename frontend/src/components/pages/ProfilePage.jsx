d28dd61910129cee0245fc64318d72b982082568
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-21 19:10:03 +0200

    add button style

--- frontend/src/components/pages/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage.jsx
@@ -12,7 +12,7 @@ const ProfilePage = () => {
 
   return (
     <>
-      <Container>
+      <Container xs="4" className='profile-page-container'>
         <Row className="profile-header">
           <Col xs="12">
             <h3>{t('profile.myProfile')}</h3>
@@ -49,16 +49,18 @@ const ProfilePage = () => {
                 <Col xs="12">
                   {t('profile.phoneNumber')}: {getUserData().phone_number}
                 </Col>
-                <div className="profile-divider"></div>
-                <Col xs="12">
-                  {t('profile.media')}: {getUserData().media_name}
-                </Col>
               </Row>
+              <div className="profile-divider"></div>
+              <Col xs="12">
+                {t('profile.media')}: {getUserData().media_name}
+              </Col>
             </>
           )}
-          <Button as={NavLink} to="/profile/edit">
+          <Col xs="12">
+          <Button variant="secondary" as={NavLink} to="/profile/edit">
             {t('profile.editProfile')}
           </Button>
+          </Col>
           <Row>
             <Col xs="12">
               {t('profile.id')}: {getUserData().uuid}
