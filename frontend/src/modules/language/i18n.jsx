86e80bd4cc7e6f76421430784f04e9532331790e
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-21 11:55:23 +0200

    add user participation request option in burger

--- frontend/src/modules/language/i18n.jsx
+++ frontend/src/modules/language/i18n.jsx
@@ -26,7 +26,8 @@ i18n
             dropDownMenuItem9: 'Support',
             dropDownMenuItem10: 'Change Language',
             dropDownMenuItem11: 'Logout',
-            dropDownMenuItem12: 'Register'
+            dropDownMenuItem12: 'Register',
+            dropDownMenuItem13: 'User participation request'
           },
           layoutPage: {
             imageHeaderText: 'WINNERS OF PREVIOUS COMPETITIONS'
@@ -286,7 +287,8 @@ i18n
             dropDownMenuItem9: 'Pagalba',
             dropDownMenuItem10: 'Keisti kalbą',
             dropDownMenuItem11: 'Atsijungti',
-            dropDownMenuItem12: 'Registruotis'
+            dropDownMenuItem12: 'Registruotis',
+            dropDownMenuItem13: 'Dalyvio užklausa'
           },
           layoutPage: {
             imageHeaderText: 'PRAEITŲ KONKURSŲ NUGALĖTOJAI'
