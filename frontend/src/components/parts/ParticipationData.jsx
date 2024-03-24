b581c6ad695cc04220f0c6d7e4e38a61be131c69
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-24 15:14:06 +0200

    center card

--- frontend/src/components/parts/ParticipationData.jsx
+++ frontend/src/components/parts/ParticipationData.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, Button, Container } from 'react-bootstrap';
+import { Card, Button, Container, Row } from 'react-bootstrap';
 import { useDropzone } from 'react-dropzone';
 
 const ParticipationData = () => {
@@ -15,23 +15,28 @@ const ParticipationData = () => {
 
   return (
     <Container>
-      <Card style={{ width: '18rem' }}>
-        <div {...getRootProps()} style={{ cursor: 'pointer' }}>
-          <input {...getInputProps()} />
-          <Card.Img variant="top" src={photo ? URL.createObjectURL(photo) : 'holder.js/100px180'} />
-        </div>
-        <Card.Body>
-          <Card.Title>{t('Card Title')}</Card.Title>
-          <Card.Text>
-            {t(
-              "Some quick example text to build on the card title and make up the bulk of the card's content."
-            )}
-          </Card.Text>
-          <Button {...getRootProps()} variant="primary">
-            Upload
-          </Button>
-        </Card.Body>
-      </Card>
+      <Row className="justify-content-center">
+        <Card style={{ width: '18rem' }}>
+          <div {...getRootProps()} style={{ cursor: 'pointer' }}>
+            <input {...getInputProps()} />
+            <Card.Img
+              variant="top"
+              src={photo ? URL.createObjectURL(photo) : 'holder.js/100px180'}
+            />
+          </div>
+          <Card.Body>
+            <Card.Title>{t('Card Title')}</Card.Title>
+            <Card.Text>
+              {t(
+                "Some quick example text to build on the card title and make up the bulk of the card's content."
+              )}
+            </Card.Text>
+            <Button {...getRootProps()} variant="primary">
+              Upload
+            </Button>
+          </Card.Body>
+        </Card>
+      </Row>
     </Container>
   );
 };
