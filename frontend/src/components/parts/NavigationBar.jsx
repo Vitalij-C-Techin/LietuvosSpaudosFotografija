43d0ba0a76c27cd9d3696fc9217eaca24fce7805
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-03-25 15:57:16 +0200

    CP1-229: navbar update for different user roles.

--- frontend/src/components/parts/NavigationBar.jsx
+++ frontend/src/components/parts/NavigationBar.jsx
@@ -1,90 +1,103 @@
 import React from 'react';
 import LanguageSwitch from './LanguageSwitch.jsx';
 import '../../modules/language/i18n.jsx';
-import { useTranslation } from 'react-i18next';
-import { NavLink, Link } from 'react-router-dom';
-import { Nav, Navbar, Dropdown } from 'react-bootstrap';
-import { useAuth } from '../context/AuthContext.jsx';
-import { IsAuthenticated, IsNotAuthenticated } from '../utils/Authentication.jsx';
+import {useTranslation} from 'react-i18next';
+import {Link, NavLink} from 'react-router-dom';
+import {Dropdown, Nav, Navbar} from 'react-bootstrap';
+import {useAuth} from '../context/AuthContext.jsx';
+import {IsAuthenticated, IsNotAuthenticated} from '../utils/Authentication.jsx';
 
 const NavigationBar = () => {
-  const { logout, getRole } = useAuth();
-  const userRole = getRole();
-  const { t } = useTranslation();
+    const {logout, getRole} = useAuth();
+    const userRole = getRole();
+    const {t} = useTranslation();
 
-  return (
-    <Navbar data-testid="NavigationBar">
-      <Nav className="ms-auto">
-        <Dropdown>
-          <Dropdown.Toggle
-            id="dropdown-basic"
-            data-testid="dropdown-menu-button"
-            variant="bg-body-tertiary"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </Dropdown.Toggle>
+    return (
+        <Navbar data-testid="NavigationBar">
+            <Nav className="ms-auto">
+                <Dropdown>
+                    <Dropdown.Toggle
+                        id="dropdown-basic"
+                        data-testid="dropdown-menu-button"
+                        variant="bg-body-tertiary"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </Dropdown.Toggle>
 
-          <Dropdown.Menu align="end">
-            <Dropdown.Item as={NavLink} to="/">
-              {t('landingPage.dropDownMenuItem1')}
-            </Dropdown.Item>
+                    <Dropdown.Menu align="end">
+                        <Dropdown.Item as={NavLink} to="/">
+                            {t('landingPage.dropDownMenuItem1')}
+                        </Dropdown.Item>
 
-            <IsNotAuthenticated>
-              <Dropdown.Item as={NavLink} to="/login">
-                {t('landingPage.dropDownMenuItem2')}
-              </Dropdown.Item>
-              <Dropdown.Item as={NavLink} to="/registration">
-                {t('landingPage.dropDownMenuItem12')}
-              </Dropdown.Item>
-            </IsNotAuthenticated>
+                        <IsNotAuthenticated>
+                            <Dropdown.Item as={NavLink} to="/login">
+                                {t('landingPage.dropDownMenuItem2')}
+                            </Dropdown.Item>
+                            <Dropdown.Item as={NavLink} to="/registration">
+                                {t('landingPage.dropDownMenuItem12')}
+                            </Dropdown.Item>
+                        </IsNotAuthenticated>
 
-            <IsAuthenticated>
-              <Dropdown.Item as={NavLink} to="/profile">
-                {t('landingPage.dropDownMenuItem3')}
-              </Dropdown.Item>
+                        <IsAuthenticated>
+                            <Dropdown.Item as={NavLink} to="/profile">
+                                {t('landingPage.dropDownMenuItem3')}
+                            </Dropdown.Item>
 
-              <Dropdown.Item as={Link} to="/" onClick={logout}>
-                {t('landingPage.dropDownMenuItem11')}
-              </Dropdown.Item>
+                            <Dropdown.Item as={Link} to="/" onClick={logout}>
+                                {t('landingPage.dropDownMenuItem11')}
+                            </Dropdown.Item>
 
-              <Dropdown.Divider />
-              <Dropdown.Item as={NavLink} to="/user-competition-list">
-                {t('landingPage.dropDownMenuItem4')}
-              </Dropdown.Item>
-              <Dropdown.Divider />
+                            {['USER', 'ADMIN', 'MODERATOR'].includes(userRole) && (
+                                <>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item as={NavLink} to="/user-competition-list">
+                                        {t('landingPage.dropDownMenuItem4')}
+                                    </Dropdown.Item>
+                                </>
+                            )}
 
-              <Dropdown.Item to="#jury-competition">
-                {t('landingPage.dropDownMenuItem5')}
-              </Dropdown.Item>
+                            {userRole === 'JURY' && (
+                                <>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item to="#jury-competition">
+                                        {t('landingPage.dropDownMenuItem5')}
+                                    </Dropdown.Item>
+                                </>
+                            )}
 
-              {userRole === 'ADMIN' && (
-                <>
-                  <Dropdown.Divider />
-                  <Dropdown.Item as={NavLink} to="/admin-manage-users">
-                    {t('landingPage.dropDownMenuItem6')}
-                  </Dropdown.Item>
-                  <Dropdown.Item as={NavLink} to="/admin-competitions-list">
-                    {t('landingPage.dropDownMenuItem7')}
-                  </Dropdown.Item>
-                  <Dropdown.Item to="#admin-category-dashboard">
-                    {t('landingPage.dropDownMenuItem8')}
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item as={NavLink} to="/admin-user-participation-requests">
-                    {t('landingPage.dropDownMenuItem13')}
-                  </Dropdown.Item>
-                </>
-              )}
-              
-            </IsAuthenticated>
-            <Dropdown.Divider />
+                            {['ADMIN', 'MODERATOR'].includes(userRole) && (
+                                <>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item as={NavLink} to="/admin-user-participation-requests">
+                                        {t('landingPage.dropDownMenuItem13')}
+                                    </Dropdown.Item>
+                                </>
+                            )}
 
-            <LanguageSwitch data-testid="change-language-menu-item" />
-          </Dropdown.Menu>
-        </Dropdown>
-      </Nav>
-    </Navbar>
-  );
+                            {userRole === 'ADMIN' && (
+                                <>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item as={NavLink} to="/admin-manage-users">
+                                        {t('landingPage.dropDownMenuItem6')}
+                                    </Dropdown.Item>
+                                    <Dropdown.Item as={NavLink} to="/admin-competitions-list">
+                                        {t('landingPage.dropDownMenuItem7')}
+                                    </Dropdown.Item>
+                                    <Dropdown.Item to="#admin-category-dashboard">
+                                        {t('landingPage.dropDownMenuItem8')}
+                                    </Dropdown.Item>
+                                </>
+                            )}
+
+                        </IsAuthenticated>
+                        <Dropdown.Divider/>
+
+                        <LanguageSwitch data-testid="change-language-menu-item"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Navbar>
+    );
 };
 
 export default NavigationBar;
