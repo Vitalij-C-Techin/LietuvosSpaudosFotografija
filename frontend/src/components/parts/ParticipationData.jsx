6bb0b439ef00d619b4010f6724065600c07e542e
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-25 14:59:38 +0200

    add image view modal

--- frontend/src/components/parts/ParticipationData.jsx
+++ frontend/src/components/parts/ParticipationData.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col, Image, Modal } from 'react-bootstrap';
 import { useDropzone } from 'react-dropzone';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -10,6 +10,8 @@ const ParticipationData = () => {
   const [tempPhotos, setTempPhotos] = useState([]);
   const [photos, setPhotos] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
 
   const onDrop = (acceptedFiles) => {
     const newTempPhotos = acceptedFiles.map(file => ({
@@ -30,6 +32,16 @@ const ParticipationData = () => {
     const photosToAdd = tempPhotos.filter(photo => photo.category === selectedCategory);
     setPhotos(prevPhotos => [...prevPhotos, ...photosToAdd]);
     setTempPhotos([]);
+  };
+
+  const handleImageClick = (imageUrl) => {
+    setModalImage(imageUrl);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModalImage(null);
   };
 
   return (
@@ -79,10 +91,23 @@ const ParticipationData = () => {
       <Row className="mt-3 justify-content-center">
         {selectedCategory && photos.filter(photo => photo.category === selectedCategory).map((photo, index) => (
           <Col key={index} className="mb-3" xs={6} md={4} lg={3}>
-            <Image src={photo.url} thumbnail />
+            <Image src={photo.url} thumbnail onClick={() => handleImageClick(photo.url)} />
           </Col>
         ))}
       </Row>
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>View Image</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {modalImage && <Image src={modalImage} fluid />}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
